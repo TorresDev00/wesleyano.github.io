a7b20831fec46b7fa2e22c4e447ad5b38366937c
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-05-21 22:00:10 -0400

    feat: change pemsun carga de fotos y avance sobre nosotros

--- assets/js/sobreNosotros.js
+++ assets/js/sobreNosotros.js
@@ -1,38 +1,6 @@
 $(document).ready(function() { 
 
 	function cambioLight(){
-		$('.nav-link').click(function(event) {
-			$('.features .nav-link').css('background', '#ffffff');
-			$('.features .nav-link').css('color', '#000000');
-			$('.features .nav-link h4').css('color', '#000');
-			$('.features .nav-link:hover').css('color', '#170c66');
-			if ($(this).hasClass('active')) {
-				$('.features .nav-link.active').css({
-					'transition': '0.3s',
-					'background': '#170c66 linear-gradient(rgb(32 48 187), rgb(25 18 97)',
-					'border-color': '#000000'
-				});
-				$('.features .nav-link.active h4').css('color', '#FFFFFF');
-				$('.features .nav-link.active i').css('color', '#FFFFFF !important');
-			}
-		});
-
-		$('.accordion-button').click(function(event) {
-			$('.accordion-button').css('background-color', '#fff');
-			$('.accordion-button').css('color', '#000');
-			$('.accordion-button i').css('color', '#000');
-			if (!$(this).hasClass('collapsed')){
-				$('.accordion-button:not(.collapsed)').css('background-color', '#170c66');
-				$('.accordion-button:not(.collapsed)').css('color', '#fff');
-				$('.accordion-button:not(.collapsed) i').css('color', '#fff');
-				$('.accordion-button:focus').css('outline', 'none');
-				$('.accordion-button:focus').css('border', 'none !important');
-				$('.accordion-button:focus').css('box-shadow', 'none !important');
-				$('.accordion-body').css('background-color', '#fff');
-				$('.accordion-body').css('border', '1px solid #ccc');
-			}
-		});
-
 		$(".dropdown").hover(function() {
 	       $('.navbar .dropdown:hover>ul').css('background', '#fff');
 	    });
@@ -46,37 +14,6 @@ $(document).ready(function() {
 	}
 
 	function cambioDark(){
-		$('.nav-link').click(function(event) {
-			$('.features .nav-link').css('background', '#101010');
-			$('.features .nav-link').css('color', '#afafaf');
-			$('.features .nav-link h4').css('color', '#afafaf');
-			$('.features .nav-link:hover').css('color', '#fff');
-			if ($(this).hasClass('active')) {
-				$('.features .nav-link.active').css({
-					'transition': '0.3s',
-					'background': '#170c66 linear-gradient(rgb(32 48 187), rgb(25 18 97)',
-					'border-color': '#000000'
-				});
-				$('.features .nav-link.active h4').css('color', '#FFFFFF');
-				$('.features .nav-link.active i').css('color', '#FFFFFF !important');
-			}
-		});
-		$('.accordion-button').click(function(event) {			
-			$('.accordion-button').css('background-color', '#000');
-			$('.accordion-button').css('color', '#fff');
-			$('.accordion-button i').css('color', '#fff');
-			if (!$(this).hasClass('collapsed')){
-				console.log('hola')
-				$('.accordion-button:not(.collapsed)').css('background-color', '#101010');
-				$('.accordion-button:not(.collapsed) i').css('color', '#fff');
-				$('.accordion-button:focus').css('outline', 'none');
-				$('.accordion-button:focus').css('border', 'none !important');
-				$('.accordion-button:focus').css('box-shadow', 'none !important');
-				$('.accordion-body').css('background-color', '#fff');
-				$('.accordion-body').css('border', '1px solid #ccc');
-			}
-		});
-
 		$(".dropdown").hover(function() {
 	       $('.navbar .dropdown:hover>ul').css('background', '#000');
 	    });
@@ -94,7 +31,7 @@ $(document).ready(function() {
 
 $('#buttonStyle').change(function() {
   if ($(this).is(':checked')) {
-  	$('body').css('background', '#101010');
+  	$('body').css('background', '#ffe600');
     $('body').css('transition', 'background 0.5s ease');
     $('a').css('color', '#2196F3');
     $('.section-bg').css('background-color', '#ffffff');
@@ -111,48 +48,21 @@ $('#buttonStyle').change(function() {
     $('.breadcrumbs').css('background-color', '#fff');
     $('.breadcrumbs h2').css('color', '#000');
     $('.breadcrumbs ol li+li').css('color', '#000');
-    $('.features p, .features h3').css('color', '#000');
-    $('.features .nav-link h4').css('color', '#000');
-    $('.features .nav-link').css('background', '#ffffff');
-    $('.features .nav-link').css('color', '#000000');
-    $('.features .nav-link:hover').css('color', '#fff');
-    $('.features .nav-link.active').css('transition', '0.3s');
-    $('.features .nav-link.active').css('background', '#170c66 linear-gradient(rgb(32 48 187), rgb(25 18 97))');
-    $('.features .nav-link.active').css('border-color', '#000000');
+    $('.sobreNosotros').css('background-color', '#ffe600');
+    $('.sobreNosotros h2').css('color', '#000');
+    $('.sobreNosotros h3').css('color', '#000');
+    $('.sobreNosotros p').css('color', '#000');
+    $('.sobreNosotros .p-1').css('color', '#fff');
     cambioLight();
-    $('.features .nav-link.active h4').css('color', '#FFFFFF');
-    $('.features .nav-link.active i').css('color', '#FFFFFF !important');
-    $('#historia').css('background', '#efefef');
-    $('.historia h2').css('color', '#000');
-    $('.historia .our-story').css('background-color', '#ffffff');
-    $('.historia .our-story').css('color', '#000');
-    $('.historia .our-story h4').css('color', '#838893');
-    $('.historia .our-story h3').css('color', '#000');
-    $('.historia ul i').css('color', '#000');
-    $('.historia .watch-video a').css('color', '#000');
-    $('.historia .watch-video i').css('color', '#000');
-    $('.accordion-button').css('background-color', '#fff');
-    $('.accordion-button').css('color', '#000');
-    $('.accordion-button i').css('color', '#000');
-    $('.accordion-item').css('border', '1px solid #fff !important');
-    $('.accordion-button:not(.collapsed)').css('background-color', '#170c66');
-    $('.accordion-button:not(.collapsed) i').css('color', '#fff');
-    $('.accordion-button:focus').css('outline', 'none');
-    $('.accordion-button:focus').css('border', 'none !important');
-    $('.accordion-button:focus').css('box-shadow', 'none !important');
-    $('.accordion-body').css('background-color', '#fff');
-    $('.accordion-body').css('border', '1px solid #ccc');
-    $('.accordion-body').css('box-shadow', '0 2px 4px rgba(0, 0, 0, 0.1)');
-    $('.preguntas').css('color', '#000');
-    $('.footer').css('background', '#e29505 linear-gradient(rgb(225 152 3), rgb(249 181 22), rgb(217 142 6))');
-    $('.footer').css('color', '#fff');
-    $('#footer .footer-top h4').css('color', '#fff');
-    $('#footer .footer-top .footer-links ul a').css('color', '#fff');
-    $('#footer .footer-top .footer-links ul i').css('color', '#fff');
-    $('.footer .footer-top h4').css('color', '#fff');
-    $('.footer .footer-top .footer-links ul a').css('color', '#fff');
-    $('.footer .footer-top .footer-links ul i').css('color', '#fff');
-    $('.footer .copyright').css('border-top', '1px solid #fff');
+    $('.footer').css('background', '#fff');
+    $('.footer').css('color', '#000');
+    $('#footer .footer-top h4').css('color', '#000');
+    $('#footer .footer-top .footer-links ul a').css('color', '#000');
+    $('#footer .footer-top .footer-links ul i').css('color', '#000');
+    $('.footer .footer-top h4').css('color', '#000');
+    $('.footer .footer-top .footer-links ul a').css('color', '#000');
+    $('.footer .footer-top .footer-links ul i').css('color', '000f');
+    $('.footer .copyright').css('border-top', '1px solid #000');
 
     $(this).next('label').find('.bi-moon-fill').addClass('d-none');
     $(this).next('label').find('.bi-sun-fill').removeClass('d-none');
@@ -182,39 +92,11 @@ $('#buttonStyle').change(function() {
     $('.breadcrumbs').css('background-color', '#101010');
     $('.breadcrumbs h2').css('color', '#fff');
     $('.breadcrumbs ol li+li').css('color', '#fff');
-    $('.features .nav-link').css('background', '#101010');
-    $('.features .nav-link').css('color', '#afafaf');
-    $('.features p, .features h3').css('color', '#fff');
-    $('.features .nav-link h4').css('color', '#afafaf');
-    $('.features .nav-link:hover').css('color', '#fff');
-    $('.features .nav-link.active').css('transition', '0.3s');
-    $('.features .nav-link.active').css('background', '#170c66 linear-gradient(rgb(32 48 187), rgb(25 18 97))');
-    $('.features .nav-link.active').css('border-color', '#000000');
+    $('.sobreNosotros').css('background-color', '#101010');
+    $('.sobreNosotros h2').css('color', '#fff');
+    $('.sobreNosotros h3').css('color', '#fff');
+    $('.sobreNosotros p').css('color', '#fff');
     cambioDark();
-    $('.features .nav-link.active h4').css('color', '#FFFFFF');
-    $('.features .nav-link.active i').css('color', '#FFFFFF !important');
-    $('#historia').css('background', '#101010');
-    $('.historia h2').css('color', '#fff');
-    $('.historia .our-story').css('background-color', '#000');
-    $('.historia .our-story').css('color', '#fff');
-    $('.historia .our-story h4').css('color', '#838893');
-    $('.historia .our-story h3').css('color', '#fff');
-    $('.historia ul i').css('color', '#fff');
-    $('.historia .watch-video a').css('color', '#fff');
-    $('.historia .watch-video i').css('color', '#fff');
-    $('.accordion-button').css('background-color', '#000');
-    $('.accordion-button').css('color', '#fff');
-    $('.accordion-button i').css('color', '#fff');
-    $('.accordion-item').css('border', '1px solid #000 !important');
-    $('.accordion-button:not(.collapsed)').css('background-color', '#170c66');
-    $('.accordion-button:not(.collapsed) i').css('color', '#fff');
-    $('.accordion-button:focus').css('outline', 'none');
-    $('.accordion-button:focus').css('border', 'none !important');
-    $('.accordion-button:focus').css('box-shadow', 'none !important');
-    $('.accordion-body').css('background-color', '#fff');
-    $('.accordion-body').css('border', '1px solid #ccc');
-    $('.accordion-body').css('box-shadow', '0 2px 4px rgba(0, 0, 0, 0.1)');
-    $('.preguntas').css('color', '#fff');
     $('.footer').css('background', '#101010');
     $('.footer').css('color', '#fff');
     $('#footer .footer-top h4').css('color', '#fff');
